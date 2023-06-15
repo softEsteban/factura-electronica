00cadd57d7d954fc3afda3f64e76a38810706584
Esteban <estebantoro.greenman@gmail.com>
2023-06-14 22:38:38 -0500

    Initializing repo with readme and js scrips

---
+++ README.md
@@ -0,0 +1,45 @@
+## Taller 2 - Construcción del XML by FACTURA LATAM
+
+[title] (https://www.youtube.com/watch?v=y6aPJGUuX-g&t=58s&ab_channel=facturalatam)
+
+**Modalidades de facturación**
+
+1. Gratuito
+2. Software propio
+3. Proveedor tecnológico
+
+### ¿Qué es XML?
+
+1. Transmitir un archivo XML de un servidor a la DIAN. (Factura electrónica)
+2. Debe tener una firma digital. (Certificado digital de la empresa que emite factura)
+
+### Diferencias entre formatos XML y JSON
+
+1. El protocolo SOAP usa XML
+
+- XML es a SOAP como JSON es a REST
+- XML tiene una estructura de etiquetas o nodos parecidos al HTML
+- JSON es una estructura de llaves y valores
+
+### Nodos/Tags de un XML de factura eléctronica
+
+1. Son los diferentes componentes de una factura que contiene los datos de la facturación. Como el usuario, el producto, la fecha y el precio.
+
+2. Estándar UBL para la validación y estructura de los datos
+
+3. En cada factura se indica el rango de facturas asignadas al cliente del certificado. Este rango se provee por la DIAN en la solicitud de una resolución.
+
+4. Es importante ser la habilitación para
+
+### Herramientas para trabajar con APIDIAN
+
+VSCode - Editor de código
+Sublime - Editor de código y archivos
+Postman - Peticiones HTTP
+SoapUI - Peticiones SOAP
+
+### Envío de XML por SOAPUI
+
+### Recepción y desempaquetado de Acuse de recibo enviado por la DIAN
+
+### Algoritmo de firma digital XADES-EPES
---
+++ factura.js
@@ -0,0 +1,102 @@
+// npm install axios xml2js
+// npm install xmlbuilder2
+const fs = require("fs");
+const axios = require("axios");
+const { parseStringPromise, XMLBuilder } = require("xml2js");
+const { create } = require("xmlbuilder2");
+
+async function generarFacturaElectronica(datosFactura) {
+  try {
+    // Paso 1: Generar el XML de la factura utilizando los datos proporcionados
+    const xmlFactura = generarXMLFactura(datosFactura);
+
+    // Paso 2: Enviar la factura electrónica a la DIAN
+    const respuestaDIAN = await enviarFacturaDIAN(xmlFactura);
+
+    // Paso 3: Procesar la respuesta de la DIAN
+    const resultado = procesarRespuestaDIAN(respuestaDIAN);
+
+    return resultado;
+  } catch (error) {
+    console.error("Error al generar la factura electrónica:", error);
+    throw error;
+  }
+}
+
+// function generarXMLFactura(datosFactura) {
+//   const builder = new Builder();
+
+//   const xmlFactura = builder.buildObject({
+//     // Estructura del XML de la factura
+//     // ...completa con los datos de la factura
+//   });
+
+//   return xmlFactura;
+// }
+
+function generarXMLFactura(datosFactura) {
+  const builder = create();
+
+  // Crear la estructura del XML de la factura
+  const xmlFactura = builder
+    .ele("Factura") // Etiqueta raíz
+    .ele("Cliente") // Etiqueta del cliente
+    .ele(
+      "Nombre",
+      datosFactura.nombreItem ? datosFactura.nombreItem.replace(/\s/g, "_") : ""
+    ) // Etiqueta del nombre del cliente con su valor
+    .up() // Volver al nivel anterior
+    .ele("Items") // Etiqueta de los items de la factura
+    .ele("Item") // Etiqueta de un item
+    .ele("Nombre", datosFactura.nombreItem.replace(/\s/g, "_")) // Reemplazar espacios por guiones bajos
+    .ele("Cantidad", datosFactura.cantidadItem) // Etiqueta de la cantidad del item con su valor
+    .end({ pretty: true }); // Finalizar y obtener el XML como una cadena formateada
+
+  return xmlFactura;
+}
+
+
+
+// Generate a timestamp-based stamp for the filename
+const timestamp = new Date().getTime();
+const filename = `factura_${timestamp}.xml`;
+
+// Write the XML to the file with the timestamp-based filename
+fs.writeFileSync(filename, xmlFactura);
+
+console.log(`XML factura has been written to '${filename}'`);
+
+async function enviarFacturaDIAN(xmlFactura) {
+  const urlDIAN = "URL_DEL_SERVICIO_WEB_DE_LA_DIAN";
+
+  const headers = {
+    // Configura los encabezados necesarios (p. ej., autorización, contenido, etc.)
+  };
+
+  const respuesta = await axios.post(urlDIAN, xmlFactura, { headers });
+  return respuesta.data;
+}
+
+async function procesarRespuestaDIAN(xmlRespuesta) {
+  const parsedResponse = await parseStringPromise(xmlRespuesta);
+
+  // Procesa la respuesta y extrae la información relevante
+  // ...completa con el procesamiento necesario
+
+  return resultado;
+}
+
+const datos = {
+  nombreCliente: "Prueba",
+  nombreItem: "Prueba_Item", // Modified item name
+  cantidadItem: 1,
+};
+const xmlFactura = generarXMLFactura(datos);
+
+// generarFacturaElectronica(datosFactura)
+//   .then((resultado) => {
+//     console.log("Factura generada correctamente:", resultado);
+//   })
+//   .catch((error) => {
+//     console.error("Error al generar la factura electrónica:", error);
+//   });
